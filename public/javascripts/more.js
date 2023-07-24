469f5d44290d1e81242bf254cc83702c60deb1ac
leonardo580 <anasbenbrahim9@gmail.com>
2023-07-24 13:47:31 +0100

    opacity

--- public/javascripts/more.js
+++ public/javascripts/more.js
@@ -1,20 +1,33 @@
 
-$(document).ready(function() {
-    const rows=document.getElementsByClassName("priority")
-    for (let i=1; i<rows.length; i++){
-    const priority=rows.item(i)
-    let color={"Low": "background-color:  #00cc66;",
-                "Normal": "background-color: #fcc00;",
-                "High" : "background-color: #ff6600;",
-                "Urgent": "background-color: #ff0000;",
-                "Immediate": "background-color: #990000;"}
-    priority.parentElement.style=color[priority.textContent]
-    priority.parentElement.style["color"]="#000000"
-    console.log(color[priority.textContent]);
-    }
 
-    
-})
+document.addEventListener("DOMContentLoaded", function() {
+    const rows = document.getElementsByClassName("priority");
+    const color = {
+        "Low": "background-color: rgba(0, 204, 102, 0.5);",
+        "Normal": "background-color: rgba(255, 204, 0, 0.5);",
+        "High": "background-color: rgba(255, 102, 0, 0.5);",
+        "Urgent": "background-color: rgba(255, 0, 0, 0.5);",
+        "Immediate": "background-color: rgba(153, 0, 0, 0.5);"
+    };
+
+    for (let i = 1; i < rows.length; i++) {
+        const priority = rows[i];
+        const priorityText = priority.textContent.trim();
+        const priorityColor = color[priorityText];
+
+        if (priorityColor) {
+            priority.parentElement.style.cssText = priorityColor;
+            priority.parentElement.style.color = "#000000";
+
+        }
+
+        // console.log(priorityColor);
+    }
+});
+
+
+
+
      
 
 
